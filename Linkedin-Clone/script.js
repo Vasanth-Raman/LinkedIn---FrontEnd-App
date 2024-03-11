7ca99e0c8bab90e8cd80779f622ee17d882afe8d
VasaR <vasanthias52@gmail.com>
2024-03-11 19:15:04 +0530

    completed and responsiveness added too

--- Linkedin-Clone/script.js
+++ Linkedin-Clone/script.js
@@ -1,5 +1,18 @@
 let profileMenu = document.getElementById("profileMenu");
 
+let sideActivity = document.getElementById("sidebarActivity");
+let moreLink = document.getElementById("showMoreLink");
+
 function toggleMenu(){
     profileMenu.classList.toggle("open-menu");
+}
+
+function toggleActivity(){
+    sideActivity.classList.toggle("open-activity");
+
+    if(sideActivity.classList.contains("open-activity")){
+        moreLink.innerHTML = "Show Less <b>-</b>";
+    }else{
+        moreLink.innerHTML = "Show more <b>+</b>";
+    }
 }
